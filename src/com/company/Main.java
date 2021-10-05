f655b5f06775750108aaf6a630dbce59339368c6
luckyjd5 <JD210029@reigate.ac.uk>
2021-10-05 10:14:25 +0100

    Updated so it shows percentage

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Random ranGen = new Random();
-        HashMap sideTally = new HashMap();
+        HashMap<Integer, Integer> sideTally = new HashMap<>();
 
         System.out.println("How many sides does your dice have?");
         int sides = input.nextInt();
@@ -20,8 +20,13 @@ public class Main {
 
         for (int i = 0; i < amtRolls; i++) {
             int ranNum = ranGen.nextInt(sides) + 1;
-            sideTally.put(ranNum, ((int) sideTally.get(ranNum) + 1));
+            sideTally.put(ranNum, (sideTally.get(ranNum) + 1));
         }
-        System.out.println(sideTally);
+        System.out.println(sideTally + "\n");
+        for (int i = 1; i < (sides + 1); i++) {
+            if ((int) sideTally.get(i) > 0) {
+                System.out.println(i + " was rolled " + ((double) sideTally.get(i) / amtRolls) * 100 + "% of the times");
+            }
+        }
     }
 }
